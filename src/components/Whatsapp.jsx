025ae88ef833a372303bc9b98b9c64cb491cc3cb
github_Praveen-webdev <praveen1598810@gmail.com>
2021-08-04 10:02:11 +0530

    add whatsapp btn

---
+++ src/components/Whatsapp.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Whatsapp() {
+	return (
+		<div className="whatsapp btn">
+			<a
+				href="https://api.whatsapp.com/send?phone=916382112038"
+				class="float"
+				target="_blank"
+			>
+				<i class="fa fa-whatsapp my-float"></i>
+			</a>
+		</div>
+	);
+}
+
+export default Whatsapp;
